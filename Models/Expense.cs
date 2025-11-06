6128a6c84c6e834574f062ccd64b2dc2ed9e6011
Jake Loew <jakeloew1@gmail.com>
2025-11-06 14:07:09 -0700

    first commit

---
+++ Models/Expense.cs
@@ -0,0 +1,19 @@
+namespace ExpenseTrackerApi.Models;
+
+public class Expense
+{
+    public long Id { get; set; }
+    /// <summary>
+    /// In USD
+    /// </summary>
+    public required decimal Amount { get; set; }
+    
+    /// <summary>
+    /// To whom the expense was paid.
+    /// </summary>
+    public required string Vendor { get; set; }
+    
+    public string Description { get; set; } = "";
+
+    public DateTime CompletedAt { get; set; }
+}
